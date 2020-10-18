6a686020393949a7e6c394fe7b72e5ef497fe53e
Addie Morrison <developer@0xADD1E.me>
2020-10-18 15:14:48 -0500

    Fix error handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod helper_structs;
+
 #[warn(clippy::pedantic)]
 #[warn(clippy::nursery)]
 #[warn(clippy::cargo)]
@@ -40,127 +42,16 @@ fn arrow_color() -> colorful::Color {
         Color::Red
     }
 }
-
-fn git() -> (String, String) {
-    #[derive(Default)]
-    struct GitFileInfo {
-        pub modified: i64,
-        pub added: i64,
-        pub deleted: i64,
-        pub copied: i64,
-    }
-    impl GitFileInfo {
-        fn add(self, chr: &char) -> Self {
-            match chr {
-                'M' => Self {
-                    modified: self.modified + 1,
-                    ..self
-                },
-                'A' => Self {
-                    added: self.added + 1,
-                    ..self
-                },
-                'D' => Self {
-                    deleted: self.deleted + 1,
-                    ..self
-                },
-                'R' | 'C' => Self {
-                    copied: self.copied + 1,
-                    ..self
-                },
-                _ => self,
-            }
-        }
-    }
-    #[derive(Default)]
-    struct GitInfo {
-        pub staged: GitFileInfo,
-        pub unstaged: GitFileInfo,
-        pub unmerged: i64,
-        pub untracked: i64,
-
-        //Branch info
-        pub ahead: i64,
-        pub behind: i64,
-        pub oid: String,
-        pub head: String,
-        pub upstream: String,
-    }
-    impl GitInfo {
-        fn add(self, line: &str) -> Self {
-            let mut words = line.split_whitespace();
-            let scan_word = words.next().unwrap_or("_");
-            match scan_word.chars().nth(0).unwrap() {
-                'f' => Self {
-                    head: String::from("FATAL"),
-                    ..self
-                }, //git failed to get info
-                'u' => Self {
-                    unmerged: self.unmerged + 1,
-                    ..self
-                },
-                '?' => Self {
-                    untracked: self.untracked + 1,
-                    ..self
-                },
-                '1' | '2' => {
-                    let chars: Vec<char> = words.next().unwrap().chars().collect();
-                    Self {
-                        staged: self.staged.add(&chars[0]),
-                        unstaged: self.unstaged.add(&chars[1]),
-                        ..self
-                    }
-                }
-                '#' => match words.next().unwrap() {
-                    "branch.oid" => Self {
-                        oid: String::from(words.next().unwrap()),
-                        ..self
-                    },
-                    "branch.head" => Self {
-                        head: String::from(words.next().unwrap()),
-                        ..self
-                    },
-                    "branch.upstream" => Self {
-                        upstream: String::from(words.next().unwrap()),
-                        ..self
-                    },
-                    "branch.ab" => Self {
-                        ahead: words
-                            .next()
-                            .unwrap()
-                            .strip_prefix('+')
-                            .unwrap()
-                            .parse()
-                            .unwrap(),
-                        behind: words
-                            .next()
-                            .unwrap()
-                            .strip_prefix('-')
-                            .unwrap()
-                            .parse()
-                            .unwrap(),
-                        ..self
-                    },
-                    _ => self,
-                },
-                '_' => self,
-                _ => self,
-            }
-        }
-    }
-    //get output
-    let output = std::process::Command::new("git")
-        .args(&["status", "--branch", "--porcelain=v2"])
-        .output()
-        .map(|a| a.stdout)
-        .unwrap_or("fatal: git failed to run".bytes().collect());
-    let info: GitInfo = String::from_utf8_lossy(&output)
-        .lines()
-        .fold(GitInfo::default(), |info, line| info.add(line));
-
+fn git_porcelain_process(porcelain_output: &str) -> (String, String) {
     use colorful::Color::{Green, LightGray, Magenta, Red, Yellow};
     use colorful::Colorful;
     use colorful::Style::Bold;
+    use helper_structs::GitInfo;
+
+    let info: GitInfo = porcelain_output
+        .lines()
+        .fold(GitInfo::default(), |info, line| info.add(line));
+
     let mut diff_seg = String::new();
     if info.untracked > 0 {
         diff_seg = format!(
@@ -233,6 +124,26 @@ fn git() -> (String, String) {
     };
     (branch_seg, diff_seg)
 }
+fn git() -> Option<(String, String)> {
+    let output = std::process::Command::new("git")
+        .args(&["status", "--branch", "--porcelain=v2"])
+        .output()
+        .map(|a| {
+            if a.stderr.len() == 0 {
+                a.stdout
+            } else {
+                a.stderr
+            }
+        });
+    let output = output.unwrap_or("fatal: git failed to run".bytes().collect());
+    let output = String::from_utf8(output);
+    let output = output.unwrap_or(String::from("fatal: git output was not UTF-8"));
+    if output.starts_with("fatal") {
+        None
+    } else {
+        Some(git_porcelain_process(&output))
+    }
+}
 
 fn main() {
     use colorful::{Color, Colorful};
@@ -242,12 +153,13 @@ fn main() {
     let path_seg = format!("<{}>", path());
     prints!(path_seg.color(Color::Cyan));
 
-    let (branch, diff) = git();
-    let branch_seg = format!("<{}>", branch);
-    prints!(branch_seg.color(Color::Violet));
+    if let Some((branch, diff)) = git() {
+        let branch_seg = format!("<{}>", branch);
+        prints!(branch_seg.color(Color::Violet));
 
-    if diff != "" {
-        print!("<{}>", diff);
+        if diff != "" {
+            print!("<{}>", diff);
+        }
     }
 
     prints!("-> ".color(arrow_color()));
